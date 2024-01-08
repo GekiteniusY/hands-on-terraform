5090bf316da6ba1ec66812caa7dbe6d851142a2c
GekiteniusY <genkichi.yogi@gmail.com>
2024-01-08 10:00:42 +0900

    terraformプロジェクトの作成

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# ---------------------------
+# 変数設定
+# ---------------------------
+variable "az_a" {
+  default = "ap-northeast-1a"
+}
+
+variable "access_key" {
+  default = "AKIATHIBFCVSVWZ7Q3NM"
+}
+
+variable "secret_key" {
+  default = "7iuv3xjY6q/mHxmF0leM99r6oGzKwyOk//HLEQMo"
+}
